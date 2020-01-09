5e6c9793f77945cc2659a80a271efaa56736350e
Alexita <alexahanthony@gmail.com>
2020-01-08 17:45:11 -0800

    added rendering functionality for each new staff member

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -11,5 +11,21 @@ getRole() {
 getOfficeNumber() {
     return this.officeNumber;
   }
-};
+  
+  cardTemplate(){
+    return `<div class="col-4">
+    <div class="card shadow p-3 mb-5" style="width: 18rem;">
+    <div class="card-body bg-warning">
+    <h5 class="card-title person-name">${this.name}</h5>
+    <p class="card-text person-title"><i class="fas fa-mug-hot"></i> Manager</p>
+    </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item person-id">ID: ${this.id}</li>
+    <li class="list-group-item person-email">Email: ${this.email}</li>
+    <li class="list-group-item person-variable">Office Number: ${this.officeNumber}</li>
+    </ul>
+    </div>
+    </div>`}
+    
+  };
 module.exports = Manager;
